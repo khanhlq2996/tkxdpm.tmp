931f70abeae453821e5975f684f3821026a2becc
Quốc Khánh Lê <quockhanh.96.bka@gmail.com>
2018-05-08 15:15:43 +0700

    Update Copyright Add "extend" for class

--- SourceCode/src/entity/DataAccessHelper.java
+++ SourceCode/src/entity/DataAccessHelper.java
@@ -1,3 +1,10 @@
+/*
+ * Phần mềm quản lý thư viện
+ * Nhóm 10 - Thiết kế và Xây dựng Phần mềm
+ * Thành viên: Đinh Thị Thu Huyền, Nguyễn Thị Ngọc Huyền, Lê Quốc Khánh
+ * Giảng viên hướng dẫn: Nguyễn Thị Thu Trang
+ * Bản quyền thuộc về Nhóm 10
+ */
 package entity;
 
 import java.io.File;
--- SourceCode/src/lendbooks/controller/LendBookController.java
+++ SourceCode/src/lendbooks/controller/LendBookController.java
@@ -1,10 +1,15 @@
+/*
+ * Phần mềm quản lý thư viện
+ * Nhóm 10 - Thiết kế và Xây dựng Phần mềm
+ * Thành viên: Đinh Thị Thu Huyền, Nguyễn Thị Ngọc Huyền, Lê Quốc Khánh
+ * Giảng viên hướng dẫn: Nguyễn Thị Thu Trang
+ * Bản quyền thuộc về Nhóm 10
+ */
 package lendbooks.controller;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Import Modal to Controller
+import entity.LendBookModal;
+
 
 /**
  *
